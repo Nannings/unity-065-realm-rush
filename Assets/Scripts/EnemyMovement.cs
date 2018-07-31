2b2ecfe6d6108782971e98edd18afbc1c00ebd7c
Nannings <mrnannings@gmail.com>
2018-07-31 18:00:42 +0200

    c 125;

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;
+
+    private void Start()
+    {
+        PrintAllWayPoint();
+    }
+
+    private void PrintAllWayPoint()
+    {
+        foreach (Block wayPoint in path)
+        {
+            print(wayPoint.name);
+        }
+    }
+}
