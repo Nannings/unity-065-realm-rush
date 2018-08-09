e6212f3a8cbf0776b89e6b66d4dac0b07e45370b
Nannings <mrnannings@gmail.com>
2018-08-09 15:28:11 +0200

    c 146;

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawn = 2f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+    private void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(secondsBetweenSpawn);
+        }
+    }
+}
